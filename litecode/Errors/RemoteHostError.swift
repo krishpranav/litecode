d4416e51b65c7075fa5bbba0560f3f9a448602e1
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-25 11:14:55 +0530

    litecode: Errors[RemoteHostError]

---
+++ litecode/Errors/RemoteHostError.swift
@@ -0,0 +1,13 @@
+//
+//  RemoteHostError.swift
+//  litecode
+//
+//  Created by Krisna Pranav on 25/04/24.
+//
+
+import Foundation
+
+enum RemoteHostError: Error {
+    case invalidUrl
+    case invalidCredentials
+}
---
+++ litecode/Errors/SourceControlError.swift
@@ -0,0 +1,8 @@
+//
+//  SourceControlError.swift
+//  litecode
+//
+//  Created by Krisna Pranav on 25/04/24.
+//
+
+import Foundation
