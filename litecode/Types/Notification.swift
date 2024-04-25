9588b6ace66adb0f7709994597f6c2aef269dc44
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-25 19:05:12 +0530

    litecode: Types[Notification]

---
+++ litecode/Types/Notification.swift
@@ -0,0 +1,38 @@
+//
+//  Notification.swift
+//  litecode
+//
+//  Created by Krisna Pranav on 25/04/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct NotificationEntry: Identifiable {
+    let id = UUID()
+    let data: NotificationData
+    var isPresented: Bool = true
+    var isRemoved: Bool = false
+}
+
+struct NotificationData {
+    let title: String
+    var source: String? = nil
+    var progress: Progress? = nil
+    let level: Level
+    let style: Style
+    
+    enum Style {
+        case basic
+        case action
+        case progress
+        case infinityProgress
+    }
+    
+    enum Level {
+        case warning
+        case info
+        case error
+        case success
+    }
+}
