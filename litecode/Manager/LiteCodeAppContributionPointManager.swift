28aae6d3c2e7b3cbdb5f0f547861f0bcd1330ae4
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-25 19:13:54 +0530

    litecode: Manager[LiteCodeAppContributionPointManager]

---
+++ litecode/Manager/LiteCodeAppContributionPointManager.swift
@@ -0,0 +1,16 @@
+//
+//  LiteCodeAppContributionPointManager.swift
+//  litecode
+//
+//  Created by Krisna Pranav on 25/04/24.
+//
+
+import Foundation
+
+protocol LiteCodeAppContributionPointManager: ObservableObject {
+    // todo
+}
+
+extension LiteCodeAppContributionPointManager {
+    // todo
+}
