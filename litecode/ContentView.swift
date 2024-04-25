2d4aeee677e0dadb90762f0a652043785a296391
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-25 10:50:45 +0530

    Initial Commit

---
+++ litecode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  litecode
+//
+//  Created by Krisna Pranav on 25/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ litecode/litecodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  litecodeApp.swift
+//  litecode
+//
+//  Created by Krisna Pranav on 25/04/24.
+//
+
+import SwiftUI
+
+@main
+struct litecodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
