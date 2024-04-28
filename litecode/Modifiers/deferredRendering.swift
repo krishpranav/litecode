b05f683ea3a3c86c57ecf8607f1de09331f71b79
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-28 16:51:17 +0530

    litecode: Modifiers[defferedRendering]

---
+++ litecode/Modifiers/deferredRendering.swift
@@ -0,0 +1,34 @@
+//
+//  deferredRendering.swift
+//  litecode
+//
+//  Created by Krisna Pranav on 28/04/24.
+//
+
+import Foundation
+import SwiftUI
+
+private struct DeferredViewModifier: ViewModifier {
+    let threshold: Double
+    
+    func body(content: Content) -> some View {
+        _content(context)
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + threshold) {
+                    self.shouldRender = true
+                }
+            }
+    }
+    
+    @ViewBuilder
+    private func _content(_ content: Content) -> some View {
+        if shouldRender {
+            content
+        } else {
+            content
+                .hidden()
+        }
+    }
+    
+    @State private var shouldRender = false
+}
